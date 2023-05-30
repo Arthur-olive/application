9b733521d87f72bc5ec52030811261b014295eea
João Arthur <joao37725@gmail.com>
2023-05-30 16:22:38 -0300

    YES

--- src/main/java/com/application/Controller/ProdutoController.java
+++ src/main/java/com/application/Controller/ProdutoController.java
@@ -1,17 +1,21 @@
 package com.application.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("produto")
 public class ProdutoController {
 
-    @GetMapping("produto/listar")
+    @GetMapping("/listar")
     public String listar(){
-        return "modulos/produtos/listar";
+        return "modulos/produto/listar";
     }
     
 
-    @GetMapping("produto/adicionar")
+    @GetMapping("/adicionar")
     public String adicionar(){
-        return "modulos/produtos/adicionar";
+        return "modulos/produto/adicionar";
     }
 }
