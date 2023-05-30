ae8b1491a8ac99510f05e5608c4c9395a741bf16
João Arthur <joao37725@gmail.com>
2023-05-30 16:47:11 -0300

    ERRO

--- src/main/java/com/application/Model/Cliente.java
+++ src/main/java/com/application/Model/Cliente.java
@@ -1,5 +1,13 @@
 package com.application.Model;
 
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+
+//@Entity
+//@Table(name = "clientes")
 public class Cliente {
+    
+  //  private int id;
+  // private String nome;
     
 }
--- src/main/java/com/application/Model/Produto.java
+++ src/main/java/com/application/Model/Produto.java
@@ -1,5 +1,26 @@
 package com.application.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "produtos")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+
 public class Produto {
-    
+        
+    @Id//javax.persistence.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidade;
 }
