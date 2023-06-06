cbd7bda4935945f8125a0443ed640c0591fece57
João Arthur <joao37725@gmail.com>
2023-06-06 15:40:06 -0300

    teste aula

--- src/main/java/com/application/Controller/ProdutoController.java
+++ src/main/java/com/application/Controller/ProdutoController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ch.qos.logback.core.model.Model;
+
 @Controller
 @RequestMapping("produto")
 public class ProdutoController {
@@ -13,9 +15,17 @@ public class ProdutoController {
         return "Modulos/Produto/listar";
     }
     
+    @GetMapping("/listar")
+    public String listar (Model model){
+        model.addAttribute("variavel_teste", "Valor da Variavel Teste");
+        return "Modulos/Produto/listar";
+
+    }
 
     @GetMapping("/adicionar")
     public String adicionar(){
         return "Modulos/Produto/adicionar";
     }
+
+
 }
