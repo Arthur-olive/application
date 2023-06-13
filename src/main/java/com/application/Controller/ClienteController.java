3b73728915d6aaa5a670461c3835805a9e7d9aa8
João Arthur <joao37725@gmail.com>
2023-06-13 15:48:23 -0300

    começando html

--- src/main/java/com/application/Controller/ClienteController.java
+++ src/main/java/com/application/Controller/ClienteController.java
@@ -1,17 +1,27 @@
 package com.application.Controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.application.Model.Cliente;
+import com.application.Model.repository.ClienteRepository;
+
 
 @Controller
 @RequestMapping("cliente")
 public class ClienteController {
 
+    @Autowired
+    private ClienteRepository repCliente;
+
+
     @GetMapping("/listar")
     public String listar (Model model){
-        model.addAttribute("variavel_teste", "Valor da Variavel Teste");
+        List<Cliente> clientes = repCliente.findAll();
+        model.addAttribute("clientes", clientes);
         return "modulos/cliente/listar";
     }
     
--- src/main/java/com/application/Controller/ProdutoController.java
+++ src/main/java/com/application/Controller/ProdutoController.java
@@ -1,16 +1,27 @@
 package com.application.Controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.application.Model.Produto;
+import com.application.Model.repository.ProdutoRepository;
+
 
 @Controller
 @RequestMapping("produto")
 public class ProdutoController {
     
+    @Autowired
+    private ProdutoRepository repProduto;
+
+
     @GetMapping("/listar")
-    public String listar(){
+    public String listar(Model model){
+        List<Produto> produtos = repProduto.findAll();
+        model.addAttribute("produtos", produtos);
         return "modulos/produto/listar";
     }
 
